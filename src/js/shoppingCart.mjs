d040b43338bb5b9158ab9aefc62180ee04ef099e
DallinS <dallinstephens1@gmail.com>
2024-07-10 19:49:04 -0600

    I am at step 5 of team activity 6 - working individually

--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -4,18 +4,46 @@ export default function shoppingCart() {
   const cartItems = getLocalStorage("cart");
 
   // console.log(cartItems);
+  // Reference for not null: https://www.geeksforgeeks.org/how-to-check-if-a-variable-is-not-null-in-javascript/
+  // "if (cartItems)" means if cartItems in not null, do this.
+  if (cartItems) {
   const cartPageParentElement = document.querySelector(".product-list");
   // console.log(cardItemTemplate);
   // console.log(cartPageParentElement);
   // console.log(cartItems);
   renderListWithTemplate(cardItemTemplate, cartPageParentElement, cartItems);
+  const total = calculateCartTotal(cartItems);
+  hideOrShowCartTotal(total)
+  }
+  // This else statement is run if cartItems is null.
+  else {
+    hideOrShowCartTotal(0);
+  }
+}
+
+function hideOrShowCartTotal(total) {
+  if (total > 0) {
+    // Reference on how to remove a class: https://www.w3schools.com/howto/howto_js_remove_class.asp
+    document.querySelector(".cart-footer").classList.remove("hide");
+    // Reference on how to add a class: https://www.w3schools.com/howto/howto_js_add_class.asp
+    document.querySelector(".cart-footer").classList.add("show");
+    document.querySelector(".cart-total").innerText = `Total: $${total}`;
+  }
+  // This means the total is 0.
+  else {
+    // Reference on how to remove a class: https://www.w3schools.com/howto/howto_js_remove_class.asp
+    document.querySelector(".cart-footer").classList.remove("show");
+    // Reference on how to add a class: https://www.w3schools.com/howto/howto_js_add_class.asp
+    document.querySelector(".cart-footer").classList.add("hide");
+  }
 }
 
 function cardItemTemplate(item) {
+  // This code was used initially: src="${item.Image}"
   return `<li class="cart-card divider">
     <a href="#" class="cart-card__image">
       <img
-        src="${item.Image}"
+        src="${item.Images.PrimaryMedium}"
         alt="${item.Name}"
       />
     </a>
@@ -26,4 +54,13 @@ function cardItemTemplate(item) {
     <p class="cart-card__quantity">qty: 1</p>
     <p class="cart-card__price">$${item.FinalPrice}</p>
   </li>`;
+}
+
+function calculateCartTotal(cartItems) {
+  const arrayWithFinalPrices = cartItems.map((cartItem) => cartItem.FinalPrice);
+  console.log(arrayWithFinalPrices);
+  // Reference on how to use array reduce(): https://www.w3schools.com/jsref/jsref_reduce.asp
+  const total = arrayWithFinalPrices.reduce((sum, itemFinalPrice) => sum + itemFinalPrice);
+  console.log(total);
+  return total;
 }
