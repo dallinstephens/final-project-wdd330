946e43998097d83373d94636c1c5f59d8672ba3e
DallinS <dallinstephens1@gmail.com>
2024-07-25 02:37:31 -0600

    Final Project - Updated Deleting Cart Items - Now Works

--- src/js/shoppingCart.mjs
+++ src/js/shoppingCart.mjs
@@ -7,63 +7,19 @@ export default function shoppingCart() {
   // Reference for not null: https://www.geeksforgeeks.org/how-to-check-if-a-variable-is-not-null-in-javascript/
   // "if (cartItems)" means if cartItems in not null, do this.
   if (cartItems) {
-  const cartPageParentElement = document.querySelector(".product-list");
-  // console.log(cardItemTemplate);
-  // console.log(cartPageParentElement);
-  // console.log(cartItems);
-  renderListWithTemplate(cardItemTemplate, cartPageParentElement, cartItems);
-  const total = calculateCartTotal(cartItems);
-  hideOrShowCartTotal(total)
+    const cartPageParentElement = document.querySelector(".product-list");
+    // console.log(cardItemTemplate);
+    // console.log(cartPageParentElement);
+    // console.log(cartItems);
+    renderListWithTemplate(cardItemTemplate, cartPageParentElement, cartItems);
+    const total = calculateCartTotal(cartItems);
+    hideOrShowCartTotal(total);
+    deleteItemUponClick();
   }
   // This else statement is run if cartItems is null.
   else {
     hideOrShowCartTotal(0);
-  }
-
-  const productListElement = document.querySelector(".product-list");
-
-  // Reference for addEventListener: https://www.w3schools.com/jsref/met_element_addeventlistener.asp
-  document.querySelector(".product-list li").addEventListener("click", function(event) {
-   // Reference for event target in JavaScript: https://www.w3schools.com/jsref/event_target.asp
-    // event.target.tagName could also be used
-    // Reference for className: https://stackoverflow.com/questions/11026056/getting-the-class-of-the-element-that-fired-an-event-using-jquery
-
-    if (event.target.className == "delete-item") {
-      // Reference on how to get index of nth element:
-      // https://stackoverflow.com/questions/54737958/get-index-of-child-elements-with-event-listener-in-javascript
-      var nodes = document.querySelectorAll('.delete-item');
-      let index = [].indexOf.call(nodes, event.target);
-      console.log(index);
-      let cartItems = getLocalStorage("cart");
-      cartItems.slice(index, 1);
-
-      setLocalStorage("cart", cartItems);
-
-
-      let numberOfCartItemsElement = document.querySelector(".number-of-cart-items");
-      
-      // Reference for array map function:
-      // https://www.w3schools.com/jsref/jsref_map.asp
-      // Reference for array reduce function:
-      // https://www.w3schools.com/jsref/jsref_reduce.asp
-      let numberOfCartItems = cartItems.reduce( (total, cartItem) => total + cartItem.Quantity, 0);
-      
-      setLocalStorage("numberOfCartItems", numberOfCartItems);
-    
-      numberOfCartItemsElement.textContent = numberOfCartItems;
-    
-      // Reference for add DOM class:
-      // https://www.w3schools.com/howto/howto_js_add_class.asp
-      numberOfCartItemsElement.classList.add("show");
-    
-       // Reference for remove DOM class:
-      // https://www.w3schools.com/howto/howto_js_remove_class.asp
-      numberOfCartItemsElement.classList.remove("hide");      
-
-      // Reference for removeChild() DOM element: https://www.w3schools.com/jsref/met_node_removechild.asp
-      productListElement.removeChild(this);
-    }    
-  });  
+  } 
 }
 
 function hideOrShowCartTotal(total) {
@@ -72,7 +28,7 @@ function hideOrShowCartTotal(total) {
     document.querySelector(".cart-footer").classList.remove("hide");
     // Reference on how to add a class: https://www.w3schools.com/howto/howto_js_add_class.asp
     document.querySelector(".cart-footer").classList.add("show");
-    document.querySelector(".cart-total").innerText = `Total: $${total}`;
+    document.querySelector(".cart-total").innerText = `Total: $${total.toFixed(2)}`;
   }
   // This means the total is 0.
   else {
@@ -98,7 +54,7 @@ function cardItemTemplate(item) {
     <a href="#" class="delete-item">Delete</a>
     <p class="cart-card__color">${item.Colors[0].ColorName}</p>
     <p class="cart-card__quantity">${item.Quantity} X $${item.FinalPrice}</p>
-    <p class="cart-card__price">$${item.Quantity * item.FinalPrice}</p>
+    <p class="cart-card__price">$${(item.Quantity * item.FinalPrice).toFixed(2)}</p>
   </li>`;
 }
 
@@ -110,4 +66,59 @@ function calculateCartTotal(cartItems) {
   const total = arrayWithFinalPrices.reduce((sum, itemFinalPrice) => sum + itemFinalPrice, 0);
   // console.log(total);
   return total;
+}
+
+
+function deleteItemUponClick() {
+  let productListElement = document.querySelector(".product-list");
+
+  // Reference for addEventListener: https://www.w3schools.com/jsref/met_element_addeventlistener.asp
+  productListElement.addEventListener("click", function(event) {
+  // Reference for event target in JavaScript: https://www.w3schools.com/jsref/event_target.asp
+    // event.target.tagName could also be used
+    // Reference for className: https://stackoverflow.com/questions/11026056/getting-the-class-of-the-element-that-fired-an-event-using-jquery
+
+    if (event.target.className == "delete-item") {
+      // Reference on how to get index of nth element:
+      // https://stackoverflow.com/questions/54737958/get-index-of-child-elements-with-event-listener-in-javascript
+      var nodes = document.querySelectorAll('.delete-item');
+      let index = [].indexOf.call(nodes, event.target);
+      console.log(index);
+      let cartItems = getLocalStorage("cart");
+      // Reference to remove item at certain index using splice:
+      // https://www.w3schools.com/jsref/jsref_splice.asp
+      cartItems.splice(index, 1);
+
+      setLocalStorage("cart", cartItems);
+
+      let numberOfCartItemsElement = document.querySelector(".number-of-cart-items");
+      
+      // Reference for array map function:
+      // https://www.w3schools.com/jsref/jsref_map.asp
+      // Reference for array reduce function:
+      // https://www.w3schools.com/jsref/jsref_reduce.asp
+      let numberOfCartItems = cartItems.reduce( (total, cartItem) => total + cartItem.Quantity, 0);
+      
+      setLocalStorage("numberOfCartItems", numberOfCartItems);
+    
+      numberOfCartItemsElement.textContent = numberOfCartItems;
+    
+      // Reference for add DOM class:
+      // https://www.w3schools.com/howto/howto_js_add_class.asp
+      numberOfCartItemsElement.classList.add("show");
+    
+      // Reference for remove DOM class:
+      // https://www.w3schools.com/howto/howto_js_remove_class.asp
+      numberOfCartItemsElement.classList.remove("hide");
+
+      let clickedLiElement = document.querySelector(`li:nth-of-type(${index + 1})`);
+
+      // Reference for removeChild() DOM element: https://www.w3schools.com/jsref/met_node_removechild.asp
+      if (cartItems) {
+        productListElement.removeChild(clickedLiElement);
+      }
+
+      shoppingCart();
+    }    
+  });
 }
